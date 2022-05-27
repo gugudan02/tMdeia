d4ced9b4cdf3a254ccf8c1b762b1c57d03682005
Simplifer <820317041@qq.com>
2022-05-27 11:01:56 +0800

    Update t2.js

--- pages/api/t2.js
+++ pages/api/t2.js
@@ -89,22 +89,6 @@ function stream2buffer(stream, type) {
     });
 }
 
-/** 定义一个函数 **/
-function getNewArray(arr, size) {  // size=5，要分割的长度
-    const arrNum = Math.ceil(arr.length / size, 10); // Math.ceil()向上取整的方法，用来计算拆分后数组的长度
-    let index = 0; // 定义初始索引
-    let resIndex = 0; // 用来保存每次拆分的长度
-    const result = [];
-    while (index < arrNum) {
-        result[index] = arr.slice(resIndex, size + resIndex);
-        resIndex += size;
-        index++;
-    }
-    return result;
-}
-
-
 let bigFile = {
-    temp: [],
-    isDone: false,
+    temp: []
 }
